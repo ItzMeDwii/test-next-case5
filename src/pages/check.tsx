970b6a725925afa2d15fbdc7fb3aeffa70fcb41d
dwikarunia0001 <dwikarunia14157375@gmail.com>
2022-10-08 11:47:15 +0700

    feat(pages): add new file check.tsx

---
+++ src/pages/check.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "../layouts/main";
+
+const check = () => {
+	return (
+		<div>
+			<h1 className="text-xl">CHECK</h1>
+		</div>
+	);
+};
+
+check.layout = MainLayout;
+
+export default check;
